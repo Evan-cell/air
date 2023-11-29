e64a94c1d0004c2aef965db8e24036449b9cf529
evan <malcomchege0582@gmail.com>
2023-11-29 16:39:36 +0300

    explore cards

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
 import Banner from '@/components/Banner'
 import Header from '@/components/Header'
+import SmallCard from '@/components/SmallCard'
 import Image from 'next/image'
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
-    <main
+    <div
     >
      <Header />
      <Banner/>
-    </main>
+     <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+      <section className='pt-6'>
+        <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+        {/* data from api with server side rendering */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {exploreData.map(({img,distance,location})=>(
+          <SmallCard 
+          key={img}
+          img={img}
+          distance={distance}
+          location={location}
+          />
+        ))}
+        </div>
+
+      </section>
+     </main>
+    </div>
   )
+}
+
+export async function getStaticProps(){
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  )
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
