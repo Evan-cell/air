c2b8e6077177766869d8d5a9f22a44ceae05e079
evan <malcomchege0582@gmail.com>
2023-12-04 15:08:04 +0300

    footer

---
+++ components/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 '>
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p> NewsRoom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Community</h5>
+            <p>Accessibility</p>
+            <p>Ausome site</p>
+            <p>Get your pretty home</p>
+            <p>Refferals accepted</p>
+            <p>Evan</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Host</h5>
+            <p>Evan</p>
+            <p>Presents</p>
+            <p>Best Dev Around</p>
+            <p>Find me on my Email</p>
+            <p>Lets Talk</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Refferal</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Call us</p>
+            <p>24hr Support</p>
+            <p>A call away</p>
+
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>Support</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Call us</p>
+            <p>24hr Support</p>
+            <p>A call away</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
